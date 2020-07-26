8feaedd688c9b989bcbbc137952b318299ab7808
MD Mohin <csemohin@gmail.com>
2020-07-26 22:01:29 +0600

    Design update
    Signed-off-by: MD Mohin <csemohin@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,14 @@
 <template>
   <div id="app">
-    <div class="quran-nav">
-      <navbar />
-      <!-- <audio controls>
-        <source
-          src="https://raw.githubusercontent.com/semarketir/quranjson/master/source/audio/001/001.mp3"
-          type="audio/mpeg"
-        />
-        Your browser does not support the audio element.
-      </audio> -->
-
-      <!-- https://raw.githubusercontent.com/semarketir/quranjson/master/source/translation/en/en_translation_1.json -->
-    </div>
-    <div class="container mt-3">
+    <div class="quran-app">
       <router-view></router-view>
     </div>
   </div>
 </template>
 
 <script>
-import Navbar from "@/views/Navbar";
 export default {
   components: {
-    Navbar,
   },
 };
 </script>
@@ -30,3 +16,29 @@ export default {
 <style lang="scss">
 @import "@/assets/scss/main.scss";
 </style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ <!-- <audio controls>
+        <source
+          src="https://raw.githubusercontent.com/semarketir/quranjson/master/source/audio/001/001.mp3"
+          type="audio/mpeg"
+        />
+        Your browser does not support the audio element.
+      </audio> -->
+
+      
+
+      <!-- https://raw.githubusercontent.com/semarketir/quranjson/master/source/translation/en/en_translation_1.json -->
